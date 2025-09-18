01c1f00da46e2a9f4bc72b4e1377ccf9ac7bb4c4
auspicious-octopus <learning.jw@proton.me>
2025-09-18 15:34:30 -0400

    updated main.py to count words

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+word_list = []
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
 
+def count_words(text):
+    word_list = text.split()
+    total_words = 0
+    for word in word_list:
+        total_words += 1
+    return total_words
+
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    print(text)
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(book_text)
+    print(f"{num_words} words found in the document")
 
 main()

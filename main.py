ef8b3de62cfdab6f1e225bd262f8a8e2cd6fef76
auspicious-octopus <learning.jw@proton.me>
2025-09-18 17:58:31 -0400

    return dictionary to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_characters
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -9,5 +10,6 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_text)
     print(f"{num_words} words found in the document")
-
+    key = count_characters(book_text)
+    print(key)
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,16 @@ def count_words(text):
     for word in word_list:
         total_words += 1
     return total_words
+
+def count_characters(text):
+    char_smol = text.lower()
+    char_list = list(char_smol)
+    char_set = set(char_list)
+    char_dict = {}
+    for char in char_set:
+        tally = 0
+        for letter in char_list:
+            if char == letter:
+                tally += 1
+        char_dict[char] = tally
+    return char_dict
